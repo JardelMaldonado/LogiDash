b84114eb0a01c2db19b96dc8df4196b4c1ea9c93
Jardel Maldonado <114194617+JardelMaldonado@users.noreply.github.com>
2026-04-09 22:38:11 -0300

    config: configuração do WebClient para consumo de API

---
+++ src/main/java/com/jardel/LogiDash/config/WebClientConfig.java
@@ -0,0 +1,22 @@
+package com.jardel.LogiDash.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Value("${profrotas.api.token}")
+    private String apiToken;
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl("https://api-portal.profrotas.com.br")
+                .defaultHeader("Content-Type", "application/json")
+                .defaultHeader("Authorization", "Bearer " + apiToken)
+                .build();
+    }
+}
