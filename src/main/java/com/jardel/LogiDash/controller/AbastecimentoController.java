6f238fc5b792c9aa34a486698631f1a7aa5fb96f
Jardel Maldonado <114194617+JardelMaldonado@users.noreply.github.com>
2026-05-25 19:04:22 -0300

    refactor: remove construtor manual do AbastecimentoController

--- src/main/java/com/jardel/LogiDash/controller/AbastecimentoController.java
+++ src/main/java/com/jardel/LogiDash/controller/AbastecimentoController.java
@@ -2,6 +2,7 @@ package com.jardel.LogiDash.controller;
 
 import com.jardel.LogiDash.dto.abastecimento.Abastecimento;
 import com.jardel.LogiDash.service.AbastecimentoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,13 +10,11 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/abastecimentos")
+@RequiredArgsConstructor
 public class AbastecimentoController {
 
     private final AbastecimentoService abastecimentoService;
 
-    public AbastecimentoController(AbastecimentoService abastecimentoService) {
-        this.abastecimentoService = abastecimentoService;
-    }
     @GetMapping("/consultar")
     public ResponseEntity<List<Abastecimento>> consultar(
             @RequestParam String dataInicio,
