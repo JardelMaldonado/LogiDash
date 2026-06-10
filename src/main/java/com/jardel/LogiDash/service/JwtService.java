316f086c9ced5d35dbbe3ee9a085509ad89d54c5
Jardel Maldonado <114194617+JardelMaldonado@users.noreply.github.com>
2026-06-10 19:24:02 -0300

    pequeno ajuste

--- src/main/java/com/jardel/LogiDash/service/JwtService.java
+++ src/main/java/com/jardel/LogiDash/service/JwtService.java
@@ -12,7 +12,7 @@ import java.util.Date;
 @Service
 public class JwtService {
 
-    @Value(" ${jwt.secret}")
+    @Value("${jwt.secret}")
     private String secret;
 
     @Value("${jwt.expiration}")
